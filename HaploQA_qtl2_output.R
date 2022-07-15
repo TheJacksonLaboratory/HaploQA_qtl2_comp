841cb3a2e46d036bfefe343b10cc466e74fe6020
Christine Lin <49139290+schristinelin@users.noreply.github.com>
2022-07-15 15:05:55 -0700

    7/15 reformatted scripts - ready for qtl2

--- HaploQA_qtl2_output.R
+++ HaploQA_qtl2_output.R
@@ -8,182 +8,137 @@ library(rstudioapi)
 library(qtl2)
 library(dplyr)
 library(tidyverse)
-library(reshape2)
-library(stats)
-library(rjson)
-library(ggplot2)
+library(httr)
+library(jsonlite)
 
+# source the function script
+root <- dirname(getSourceEditorContext()$path)
+source(paste0(root,"/web_scrape_functions.R"))
 
 #### change the below params
 dir_name <- 'haploqa_collab_cross' # enter directory name
 sample_type <- 'Collaborative Cross' # Collaborative Cross/MiniMUGA/GigaMUGA
 output_dir_name <- 'cc_qtl2_test'
-file_gen = TRUE
 
-# source functions used
-root <- dirname(getSourceEditorContext()$path)
-source(paste0(root,"/web_scrape_functions.R"))
+#### Set toggles
+generate_summary <- TRUE # set to TRUE to generate summary table
+output_summary <- TRUE # set to TRUE to output summary table to data directory
+generate_sample_individuals <- TRUE # set to TRUE to generate individual sample data
+output_samples <- TRUE 
+qtl2_file_gen = TRUE
 
 
+### Part 1 - get results from HaploQA
+# data output directory
+data_dir <- file.path(root, dir_name)
+dir.create(data_dir, showWarnings = FALSE) 
+
+# set main URL domain
+url_domain <- 'https://haploqa.jax.org' 
+# target sample URL
+sample_url <- 'https://haploqa.jax.org/tag/Collaborative%20Cross.html' # change to the URL of sample
+
+#### html file prep
+# read html file
+haploqa_html <- read_html(sample_url)
+
+# extract information from html file
+html_temp <- haploqa_html %>% html_nodes("a") %>% html_attr("href")
+url_list <- paste0(url_domain, html_temp[grepl('/sample', html_temp)])
+
+#### implementations
+# summary table
+if (generate_summary == TRUE) {
+  print(paste0('Working on summary file:'))
+  sum_df <- sample_summary_scrape(haploqa_html, url_list)
+  if (output_summary == TRUE) {
+    print('Writing to directory')
+    write.csv(sum_df, paste0(data_dir, '/collaborative_cross_summary.csv'), row.names = FALSE)
+  }
+}
+
+# individual samples
+inc = 0
+for (url in url_list) {
+  #url <- url_list[1]
+  inc = inc + 1 # increment
+  if (generate_sample_individuals == TRUE) {
+    file <- sample_individual_scrape(url, url_domain)
+    print(paste0('Working on file ', inc, '/66: ', file))
+    sample_df_save <- as.data.frame(content(GET(file)))
+    if (output_samples == TRUE) {
+      print('Writing to directory')
+      file_name <- unlist(strsplit(file, '/'))[5]
+      GET(file, write_disk(paste0(data_dir, '/', file_name)))
+    }
+  }
+}
+
+
+### Part 2 - convert results to qtl2 input format
 ## create a data directory for qtl2 input data
 qtl2_dir <- file.path(root, output_dir_name) # name of desired output folder
 # create if folder not exist
 dir.create(qtl2_dir, showWarnings = FALSE)
 
-
-# function to generate files
-get_qtl2_input <- function(dir_name, sample_type, output_dir_name){
-  # list to store outputs
-  file_output <- list()
-  
-  ### get all data from directory
-  config <- fread(paste0(root, '/annotations_config.csv')) %>% filter(array_type == sample_type)
-  annot_file <- config$annot_file
-  dict_file <- config$dict_file
-  
-  # direct to the data directory filepath
-  data_dir <- file.path(root, dir_name)
-  
-  # all txt file from directory
-  data_files <- dir(data_dir, pattern = '\\.txt$', full.names = TRUE)
-  summary_file <- dir(data_dir, pattern = '\\.csv$', full.names = TRUE)
-  
-  ### combine all files
-  df_all <- rbindlist(lapply(data_files, fread)) # use sample_id or original?
-  
-  ### genotype data
-  geno_sub <- df_all %>% select(sample_id, snp_id, allele1_fwd, allele2_fwd) %>%
-    mutate(gene_exp = paste(allele1_fwd, allele2_fwd, sep = '')) %>%
-    select(sample_id, snp_id, gene_exp)
-  df_geno <- dcast(geno_sub, snp_id~sample_id, value.var="gene_exp") # inbred - most are homozygous
-  #write.csv(df_geno, paste0(qtl2_dir, '/test_geno.csv'), row.names = F)
-  file_output[[1]] <- df_geno
-  
-  ### gmap and pmap 
-  ### annotations
-  dir <- "https://raw.githubusercontent.com/kbroman/MUGAarrays/main/UWisc/" # always the same
-  annot_df <- read.csv(paste0(dir, annot_file))
-  annot_dict <- read.csv(paste0(dir, dict_file))
-  # extract only columns needed from sample file
-  map_df <- df_all %>% 
-    select(snp_id, chromosome, position_bp) %>%
-    rename(marker = snp_id, chr = chromosome) %>% unique()
-  # extract columns from annotations
-  annot_map <- annot_df %>% select(marker, chr, bp_mm10, cM_cox) %>% unique()
-  # merge the two dataframes accordingly
-  map_all <- merge(map_df, annot_map, by = c('marker')) # NAs are arrays to detect mutations
-  # hence, drop NAs (for now)
-  
-  ## genetic map data (pos in cM)
-  # drop NAs, only keep chromosomes that combine 
-  df_gmap <- map_all %>% select(marker, chr.x, cM_cox) %>% rename(chr = chr.x, pos = cM_cox) %>% drop_na()
-  file_output[[2]] <- df_gmap
-  ## physical map data (pos in Mbp)
-  df_pmap <- map_all %>% select(marker, chr.x, bp_mm10) %>% rename(chr = chr.x, pos = bp_mm10) %>%
-    mutate(pos = pos/1000000) %>% drop_na()
-  ### check if gmap and pmap - intersect to make sure markers are consistent
-  file_output[[3]] <- df_pmap
-  
-  ## phenotype data
-  # simulate using rnorm/runiform
-  df_pheno <- df_all %>% select(sample_id) %>% unique()
-  df_pheno$WBC <- floor(runif(nrow(df_pheno), 1, 9)) # copying Dan's work from 2014
-  df_pheno$NEUT <- floor(rnorm(nrow(df_pheno), 800, 500))
-  file_output[[4]] <- df_pheno
-  
-  # covariate file - map to sex
-  # sample_id, sex
-  sum_df <- fread(summary_file) %>% 
-    select(ID, Sex, `% Het. Calls`, `% No Call`) %>% rename(sample_id = ID)
-  df_cov_sub <- df_all %>% select(sample_id, chromosome)
-  df_cov_all <- merge(sum_df, df_cov_sub, by = 'sample_id')
-  df_cov_all$`% Het. Calls` <- as.numeric(gsub("%", "", df_cov_all$`% Het. Calls`))
-  df_cov_all$`% No Call` <- as.numeric(gsub("%", "", df_cov_all$`% No Call`))
-  # new column
-  df_cov_all$calc_sex <- NA
-  # set cutoffs
-  het_cutoff <- as.numeric(quantile(df_cov_all$`% Het. Calls`, 0.4))
-  nocall_cutoff_lower <- as.numeric(quantile(df_cov_all$`% No Call`, 0.4))
-  nocall_cutoff_higher <- as.numeric(quantile(df_cov_all$`% No Call`, 0.8))
-  # females
-  df_cov_all$calc_sex[(df_cov_all$`% Het. Calls` >= het_cutoff) & (df_cov_all$`% No Call` >= nocall_cutoff_higher)] <- 'female'
-  # males
-  df_cov_all$calc_sex[(df_cov_all$`% Het. Calls` <= het_cutoff) & (df_cov_all$`% No Call` <= nocall_cutoff_lower)] <- 'male'
-  #df_cov_all[!is.na(df_cov_all$calc_sex),]
-  
-  df_covar <- df_cov_all %>% select(sample_id, Sex) %>% rename(id = sample_id) %>% unique()
-  file_output[[5]] <- df_covar
-  
-  ### founder genomes
-  founders_dict <- fread(paste0(root, '/founder_strains_table.csv')) %>% rename(`Strain Name` = founder_strain)
-  founder_strains <- unique(founders_dict$`Strain Name`)
-  # web scraping for founder sample 'UNC_Villena_GIGMUGV01_20141012_FinalReport'
-  # set urls
-  founder_url <- 'https://haploqa.jax.org/tag/UNC_Villena_GIGMUGV01_20141012_FinalReport.html'
-  url_domain <- 'https://haploqa.jax.org' 
-  # get html and list of url to sample data
-  html_table <- read_html(founder_url) %>% html_nodes("a") %>% html_attr("href")
-  url_list <- paste0(url_domain, html_table[grepl('/sample', html_table)])
-  # loop through urls to extract individual files
-  founders_total = data.frame() # container
-  for (url in url_list) {
-    file <- sample_individual_scrape(url) # call function from another script
-    df_temp <- as.data.frame(content(GET(file)))
-    founders_total <- rbind(founders_total,df_temp)
-  }
-  
-  # founder strains all have secondary ID with '_'
-  founders_df <- founders_total[grep("_", founders_total$original_sample_id), ] %>% 
-    mutate(gene_exp = paste(allele1_fwd, allele2_fwd, sep = '')) %>%
-    select('sample_id', 'snp_id', 'gene_exp')
-  
-  sum_df <- sample_summary_scrape(read_html(founder_url), url_list) %>% 
-    select('ID', 'Strain Name') %>% rename(sample_id = ID)
-  founders_test <- merge(founders_df, sum_df, by = 'sample_id')
-  
-  founders_test <- merge(founders_test, founders_dict, by = 'Strain Name')
-  test_foundergeno <- founders_test %>% select(snp_id, letter, gene_exp) %>% 
-    rename(strain = letter, marker = snp_id)
-  test_foundergeno <- dcast(test_foundergeno, marker~strain, value.var="gene_exp")
-  file_output[[6]] <- test_foundergeno 
-  
-  # control file
-  # see if read_cross2 function works
-  ## skip insert
-  # calc_genoprob
-  
-  return(file_output)
-}
-
-### 1. overall no-calls can not be more than 10%
-### no-calls on Y chrom for sex - fill in unknowns, and validate the known ones.
-
 # implement function
-file_output <- get_qtl2_input(dir_name, sample_type, output_dir_name)
+file_output <- get_qtl2_input(dir_name, sample_type, output_dir_name, sum_df)
 
 # unpack
-if (file_gen == TRUE) {
+if (qtl2_file_gen == TRUE) {
   df_geno <- file_output[[1]]
   write.csv(df_geno, paste0(qtl2_dir, '/test_geno.csv'), row.names = F)
   df_gmap <- file_output[[2]]
   write.csv(df_gmap, paste0(qtl2_dir, '/test_gmap.csv'), row.names = F)
   df_pmap <- file_output[[3]]
   write.csv(df_pmap, paste0(qtl2_dir, '/test_pmap.csv'), row.names = F)
-  df_pmap <- file_output[[4]]
-  write.csv(df_pmap, paste0(qtl2_dir, '/test_pheno.csv'), row.names = F)
-  df_pmap <- file_output[[5]]
-  write.csv(df_pmap, paste0(qtl2_dir, '/test_covar.csv'), row.names = F)
-  df_pmap <- file_output[[6]]
-  write.csv(df_pmap, paste0(qtl2_dir, '/test_foundergeno.csv'), row.names = F)
+  df_pheno <- file_output[[4]]
+  write.csv(df_pheno, paste0(qtl2_dir, '/test_pheno.csv'), row.names = F)
+  df_covar <- file_output[[5]]
+  write.csv(df_covar, paste0(qtl2_dir, '/test_covar.csv'), row.names = F)
+  df_foundergeno <- file_output[[6]]
+  write.csv(df_foundergeno, paste0(qtl2_dir, '/test_foundergeno.csv'), row.names = F)
+  df_crossinfo <- file_output[[7]]
+  # temporary fix - NA can't be here
+  df_crossinfo <- df_crossinfo %>% select(-ID) %>% unique() %>% drop_na()
+  write.csv(df_crossinfo, paste0(qtl2_dir, '/test_crossinfo.csv'), row.names = F)
 }
 
-### view some files
-minimuga_dir <- "/Users/linc/Documents/GitHub/HaploQA_qtl2_comp/MiniMUGA_qtl2_test"
-cc_dir <- "/Users/linc/Documents/GitHub/HaploQA_qtl2_comp/cc_qtl2_test"
-file_type <- 'pmap'
-cc_sum <- fread(paste0(data_dir, '/collaborative_cross_summary.csv'))
+### control file
+control_fp <- paste0(qtl2_dir, '/test.json')
+file.create(control_fp)
 
-minimuga_test <- fread(paste0(minimuga_dir, '/test_', file_type,'.csv'))
-cc_test <- fread(paste0(cc_dir, '/test_', file_type,'.csv'))
-head(minimuga_test)
-head(cc_test)
+control_file <- '{
+  "description": "HaploQA data - qtl2 test run",
+  "crosstype": "risib8",
+  "sep": ",",
+  "na.strings": ["-", "NA"],
+  "comment.char": "#",
+  "geno": "test_geno.csv",
+  "founder_geno": "test_foundergeno.csv",
+  "gmap": "test_gmap.csv",
+  "pmap": "test_pmap.csv",
+  "pheno": "test_pheno.csv",
+  "covar": "test_covar.csv",
+  "x_chr": "X",
+  "genotypes": {
+    "A": 1,
+    "H": 2,
+    "B": 3
+  },
+  "geno_transposed": true,
+  "founder_geno_transposed": true,
+  "sex": {
+    "covar": "Sex",
+    "female": "female",
+    "male": "male"
+  },
+  "cross_info": {
+    "file": "test_crossinfo.csv"
+  }
+}'
+
+writeLines(control_file, control_fp)
+
+cc_test <- read_cross2(control_fp)
+
--- web_scrape_functions.R
+++ web_scrape_functions.R
@@ -1,9 +1,16 @@
 library(rvest)
 library(httr)
 library(data.table)
+library(qtl2convert)
+library(ggrepel)
+library(dplyr)
+library(tidyverse)
+library(reshape2)
+library(stats)
+library(ggplot2)
 
 # scape function - needs rewriting, it's also in another script
-sample_individual_scrape <- function(url) {
+sample_individual_scrape <- function(url, url_domain) {
   # read html of website with sample data
   html_sample <- read_html(url)
   # get class 'btn'
@@ -31,4 +38,179 @@ sample_summary_scrape <- function(html_file, url_list) {
   sum_table$`Sample Filepath` <- url_list
   
   return(sum_table)
+}
+
+
+# function to generate files
+get_qtl2_input <- function(dir_name, sample_type, output_dir_name, summary_df){
+  # list CONTAINER to store outputs
+  file_output <- list()
+  
+  ### get all data from directory
+  # read the config file
+  config <- fread(paste0(root, '/annotations_config.csv'))
+  config <- config %>% filter(array_type == sample_type)
+  annot_file <- config$annot_file
+  dict_file <- config$dict_file
+  
+  ### annotations
+  dir <- "https://raw.githubusercontent.com/kbroman/MUGAarrays/main/UWisc/" # always the same
+  annot_df <- read.csv(paste0(dir, annot_file))
+  annot_dict <- read.csv(paste0(dir, dict_file))
+  
+  # direct to the data directory filepath
+  data_dir <- file.path(root, dir_name)
+  
+  # all txt file from directory
+  data_files <- dir(data_dir, pattern = '\\.txt$', full.names = TRUE)
+  #summary_file <- dir(data_dir, pattern = '\\.csv$', full.names = TRUE)
+  
+  ### combine all files
+  df_all <- rbindlist(lapply(data_files, fread)) # use sample_id or original?
+  
+  ### genotype data
+  geno_sub <- df_all %>% select(sample_id, snp_id, allele1_fwd, allele2_fwd) %>%
+    mutate(gene_exp = paste(allele1_fwd, allele2_fwd, sep = '')) %>%
+    select(sample_id, snp_id, gene_exp) %>% rename(marker = snp_id)
+  df_geno <- dcast(geno_sub, marker~sample_id, value.var="gene_exp") # inbred - most are homozygous
+  # get marker annotations from wisc
+  # encode_genome function on geno and founder from qtl2 convert package - args: matrix of genotypes, matrixs of two alleles
+  annot_encode_df <- annot_df %>% select(marker, chr, snp) %>%
+    separate(snp, c("allele1", "allele2"), sep=cumsum(c(1)))
+  # put alleles in order of marker as shown in geno dataframe
+  geno_encode_annot <- merge(df_geno, annot_encode_df, by = 'marker') %>% select(allele1, allele2)
+  geno_encode_annot <- find_unique_geno(geno_encode_annot, na.strings = c('NA', '-', ""))
+  rownames(df_geno) <- df_geno$marker
+  df_geno_encoded <- as.data.frame(encode_geno(df_geno[,-1], geno_encode_annot))
+  df_geno_encoded$marker <- rownames(df_geno_encoded)
+  # reorder columns
+  df_geno_encoded <- df_geno_encoded %>% select(marker, everything())
+  file_output[[1]] <- df_geno_encoded 
+  print('genotype data done')
+  
+  ### gmap and pmap 
+  # extract only columns needed from sample file
+  map_df <- df_all %>% 
+    select(snp_id, chromosome, position_bp) %>%
+    rename(marker = snp_id, chr = chromosome) %>% unique()
+  # extract columns from annotations
+  annot_map <- annot_df %>% select(marker, chr, bp_mm10, cM_cox) %>% unique()
+  # merge the two dataframes accordingly
+  map_df <- merge(map_df, annot_map, by = c('marker')) %>% drop_na() # NAs are arrays to detect mutations
+  # hence, drop NAs (for now)
+  
+  ## genetic map data (pos in cM)
+  # drop NAs, only keep chromosomes that combine 
+  df_gmap <- map_df %>% select(marker, chr.x, cM_cox) %>% rename(chr = chr.x, pos = cM_cox) %>% drop_na()
+  file_output[[2]] <- df_gmap
+  print('genetic map data done')
+  ## physical map data (pos in Mbp)
+  df_pmap <- map_df %>% select(marker, chr.x, bp_mm10) %>% rename(chr = chr.x, pos = bp_mm10) %>%
+    mutate(pos = pos/1000000) %>% drop_na()
+  ### check if gmap and pmap - intersect to make sure markers are consistent
+  file_output[[3]] <- df_pmap
+  print('physical map data done')
+  
+  ## phenotype data
+  # simulate using rnorm/runiform
+  df_pheno <- df_all %>% select(sample_id) %>% unique()
+  df_pheno$WBC <- floor(runif(nrow(df_pheno), 1, 9)) # copying Dan's work from 2014
+  df_pheno$NEUT <- floor(rnorm(nrow(df_pheno), 800, 500))
+  file_output[[4]] <- df_pheno
+  print('phenotype data done')
+  
+  # covariate file - map to sex
+  #summary_df <- fread(summary_file)
+  # sample_id, sex
+  sum_df <- summary_df %>% 
+    select(ID, Sex, `% Het. Calls`, `% No Call`) %>% rename(sample_id = ID)
+  df_cov_all <- merge(sum_df, df_all %>% select(sample_id, chromosome, allele1_fwd, allele2_fwd), by = 'sample_id')
+  # convert to numerics
+  df_cov_all$`% Het. Calls` <- as.numeric(gsub("%", "", df_cov_all$`% Het. Calls`))
+  df_cov_all$`% No Call` <- as.numeric(gsub("%", "", df_cov_all$`% No Call`))
+  # remove those with overall no calls more than 10%
+  df_cov_all <- df_cov_all[!df_cov_all$`% No Call` > 10,] %>% select(sample_id, Sex, chromosome, allele1_fwd, allele2_fwd)
+  df_cov_all_xy <- df_cov_all[df_cov_all$chromosome %in% c('X', 'Y'),]
+  # count het_x and nc_y
+  nc_y <- df_cov_all_xy[(df_cov_all_xy$allele1_fwd == '-') & (df_cov_all_xy$allele2_fwd == '-'),] %>% # both are '-', no call
+    filter(chromosome == 'Y') %>%
+    group_by(sample_id) %>% summarise(NC_Y = n())
+  het_x <- df_cov_all_xy[(df_cov_all_xy$allele1_fwd != df_cov_all_xy$allele2_fwd),] %>% # heterozygous
+    filter(chromosome == 'X') %>%
+    group_by(sample_id) %>% summarise(Het_X = n())
+  plot_all <- merge(nc_y, het_x, by = 'sample_id')
+  # scatterplot to show results
+  ggplot(plot_all) + aes(x = Het_X, y = NC_Y, label = sample_id) + geom_point() + 
+    geom_text_repel(size=3, max.overlaps = 20) + ggtitle('SNP count - Heterozygous on X vs. No call on Y in each sample')
+  # split genders
+  females_list <- plot_all[plot_all$NC_Y > 75,]$sample_id
+  males_list <- plot_all[plot_all$NC_Y < 75,]$sample_id
+  # create new column
+  df_cov_all$calc_sex <- NA
+  # calculated gender
+  df_cov_all$calc_sex[df_cov_all$sample_id %in% females_list] <- 'female'
+  df_cov_all$calc_sex[df_cov_all$sample_id %in% males_list] <- 'male'
+  # list of difference in calculated gender and original
+  sex_diff <- unique(df_cov_all[(df_cov_all$Sex != df_cov_all$calc_sex),]$sample_id)
+  print(paste0('Samples with different calculated gender than original entered: ', paste(sex_diff, collapse=', ' )))
+  # select columns needed
+  df_covar <- df_cov_all %>% select(sample_id, calc_sex) %>% rename(id = sample_id, Sex = calc_sex) %>% unique()
+  file_output[[5]] <- df_covar
+  print('phenotype covariate data done')
+  
+  ### founder genomes
+  founders_dict <- fread(paste0(root, '/founder_strains_table.csv')) %>% rename(`Strain Name` = founder_strain)
+  founder_strains <- unique(founders_dict$`Strain Name`) # get the names of founder strains
+  # web scraping for founder sample 'UNC_Villena_GIGMUGV01_20141012_FinalReport'
+  # set urls
+  founder_url <- 'https://haploqa.jax.org/tag/UNC_Villena_GIGMUGV01_20141012_FinalReport.html'
+  url_domain <- 'https://haploqa.jax.org'
+  # get html and list of url to sample data
+  html_table <- read_html(founder_url) %>% html_nodes("a") %>% html_attr("href")
+  url_list <- paste0(url_domain, html_table[grepl('/sample', html_table)])
+  iter_len <- length(url_list)
+  # loop through urls to extract individual files
+  founders_total = data.frame() # container
+  inc = 0
+  for (url in url_list) {
+    file <- sample_individual_scrape(url, url_domain) # call function from another script
+    inc = inc+1 # track progress
+    print(paste0('working on file ', inc, '/', iter_len, ' ', file))
+    df_temp <- as.data.frame(content(GET(file)))
+    founders_total <- rbind(founders_total,df_temp)
+  }
+  
+  # founder strains all have secondary ID with '_'
+  founders_df <- founders_total[grep("_", founders_total$original_sample_id), ] %>% 
+    mutate(gene_exp = paste(allele1_fwd, allele2_fwd, sep = '')) %>%
+    select('sample_id', 'snp_id', 'gene_exp')
+  
+  sum_df <- sample_summary_scrape(read_html(founder_url), url_list) %>% 
+    select('ID', 'Strain Name') %>% rename(sample_id = ID)
+  founders_test <- merge(founders_df, sum_df, by = 'sample_id')
+  
+  founders_test <- merge(founders_test, founders_dict, by = 'Strain Name')
+  test_foundergeno <- founders_test %>% select(snp_id, letter, gene_exp) %>% 
+    rename(strain = letter, marker = snp_id)
+  test_foundergeno <- dcast(test_foundergeno, marker~strain, value.var="gene_exp")
+  rownames(test_foundergeno) <- test_foundergeno$marker
+  # put the allele annotations in according order
+  founder_encode_annot <- merge(annot_encode_df, test_foundergeno, by = 'marker') %>% select(allele1, allele2)
+  df_founders_encoded <- as.data.frame(encode_geno(test_foundergeno[,-1], founder_encode_annot))
+  df_founders_encoded$marker <- rownames(df_founders_encoded)
+  df_founders_encoded <- df_founders_encoded %>% select(marker, everything())
+  file_output[[6]] <- df_founders_encoded 
+  print('founder geno data done')
+  
+  # cross info
+  # join with summary table and CC file in karl's github
+  # match the strain names with the cc cross info csv
+  ci_temp <- read.csv('https://raw.githubusercontent.com/rqtl/qtl2data/main/CC/cc_crossinfo.csv', skip=3) %>% 
+    separate(id, c("Strain Name", "sec_id"), "/") %>% select(-sec_id)
+  ci_sum <- summary_df %>% select(`Strain Name`, `ID`)
+  df_crossinfo <- merge(ci_temp, ci_sum, by = 'Strain Name', all.y = T)
+  file_output[[7]] <- df_crossinfo 
+  print('cross info data done')
+  
+  return(file_output)
 }
